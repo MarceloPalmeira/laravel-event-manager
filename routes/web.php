cbc4b3349918215c7e9892a4b3a0bc54babfa0ca
Marcelo Palmeira <130473820+MarceloPalmeira@users.noreply.github.com>
2025-01-30 08:24:06 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//index() = Listar todos os registros de uma entidade no banco de dados.
+//create() =  Ele não salva nada. Apenas retorna uma view com os campos de um formulário para o usuário preencher.
+//store() =Tem como objetivo receber os dados do formulário (enviados via POST), validá-los e criar efetivamente um novo registro no banco.
+//show($id) = Exibir os detalhes de um registro específico.
+//edit($id) =  Exibir um formulário de edição de um registro.
+//update($id) = Receber os dados do formulário de edição (geralmente via PUT ou PATCH), validá-los e atualizar o registro no banco.
+
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
